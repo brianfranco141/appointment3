d43e137613e80f69f9f8207c7787e209598e0249
Brian Franco <42521586+brianfranco141@users.noreply.github.com>
2022-02-22 22:39:12 -0800

    Add files via upload

--- contracts/appointment3.sol
+++ contracts/appointment3.sol
@@ -2,6 +2,19 @@
 pragma solidity ^0.8.0;
 
 contract appointment3 {
+    uint rate;
+    address owner;
 
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function getRate() public view returns (uint) {
+        return rate;
+    }
+    
+    function setRate(uint _rate) public {
+        rate = _rate;
+    }
 }
 
